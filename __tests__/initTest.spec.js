b431e1b501c4e9fa124fdd98ca8cc4d73007be30
jeremyengland <jwengland2808@gmail.com>
2020-11-16 13:18:02 -0800

    Initialized testing suite (Jest) and ran first test

---
+++ __tests__/initTest.spec.js
@@ -0,0 +1,24 @@
+xdescribe("Suite Installation Check", () => {
+
+  test("it should pass for a passing test", () => {
+    const input = [1, 2, 3];
+    const output = 3;
+
+    const returnLastNum = (arr) => {
+      return arr[arr.length - 1];
+    }
+
+    expect(returnLastNum(input)).toEqual(output);
+  });
+
+  test("it should fail for a failing test", () => {
+    const input = [1, 2, 3, 4];
+    const output = 3;
+
+    const returnLastNum = (arr) => {
+      return arr[arr.length - 1];
+    }
+
+    expect(returnLastNum(input)).toEqual(output);
+  });
+});
